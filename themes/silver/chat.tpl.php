ff14dfb5ebfec6c9a72cdefdfee05f41a9a1a8c2
Jack Harley <jackpharley@gmail.com>
2011-04-19 13:53:51 +0100

    New theme, better AJAX, etc, etc

---
+++ themes/silver/chat.tpl.php
@@ -0,0 +1,8 @@
+{% extends base %}
+
+{% block pageTitle %}Webchat{% endblock %}
+
+{% block content %}
+<h1>Webchat</h1>
+<iframe width="100%" height="90%" src="http://widget.mibbit.com/?settings=5f7ba721eda38af8b5cafb392ec80c84&server={{address}}&channel={{channel|hashtohtml}}"></iframe>
+{% endblock %}
