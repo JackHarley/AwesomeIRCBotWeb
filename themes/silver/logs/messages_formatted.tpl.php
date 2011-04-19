70460d76d5f07e5c045f201bb5f7426a886dff63
Jack Harley <jackpharley@gmail.com>
2011-04-19 17:32:20 +0100

    Updated logs page to display all the fanciness added to the channel right now page

--- themes/silver/logs/messages_formatted.tpl.php
+++ themes/silver/logs/messages_formatted.tpl.php
@@ -10,8 +10,24 @@
 		{% for message in messages %}
 			<tr>
 				<td>{{message.time|timetodate:"d M Y H:i:s"}}</td>
-				<td><a href="{% url /index.php/stats/user %}/{{message.nickname}}"><b>&lt;{{message.nickname}}&gt;</b></a>&nbsp;&nbsp;</td>
-				<td>{{message.message}}</td>
+				{% if message.type == 4412 %}
+					{% if message.nickname != " " %}
+						<td><a href="{% url /index.php/stats/user %}/{{message.nickname}}"><b>&lt;{{message.nickname}}&gt;</b></a>&nbsp;&nbsp;</td>
+					{% else %}
+						<td></td>
+					{% endif %}
+					<td>{{message.message|urlize}}</td>
+				{% else if message.type == 421 %}
+					<td></td>
+					<td>{{message.nickname}} joined the channel</td>
+				{% else if message.type == 422 %}
+					<td></td>
+					<td>{{message.nickname}} left the channel</td>
+				{% else if message.type == 412 %}
+					<td></td>
+					<td>{{message.nickname}} changed nickname to {{message.target_nick}}</td>
+				{% endif %}
+				
 				<td>&nbsp;&nbsp;</td>
 			</tr>
 		{% endfor %}
