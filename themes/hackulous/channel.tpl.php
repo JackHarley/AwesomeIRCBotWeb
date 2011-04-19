f424b7c8840484d6dd1edf02bcb70e98fdd1fe3e
AwesomezGuy <jackpharley@gmail.com>
2011-04-19 14:08:14 +0100

    Updated the Hackulous theme for the new AJAX

--- themes/hackulous/channel.tpl.php
+++ themes/hackulous/channel.tpl.php
@@ -2,6 +2,46 @@
 
 {% block pageTitle %}Channel{% endblock %}
 
+{% block jsTwo %}
+<script type="text/javascript">
+	var lpOnComplete = function(response) {
+		var jsonObj = response;
+		if (jsonObj.type == 4412) {
+			if (jsonObj.nickname != " ") {
+				var html = '<tr><td><b><a href="{% url /index.php/stats/user %}/'+jsonObj.nickname+'">&lt;'+jsonObj.nickname+'&gt;</a></b></td><td>'+jsonObj.message+'</td></tr>';
+			}
+			else {
+				var html = '<tr><td></td><td>'+jsonObj.message+'</td></tr>';
+			}
+		}
+		else if (jsonObj.type == 421) {
+			var html = '<tr><td></td><td>'+jsonObj.nickname+' joined the channel</td></tr>';
+		}
+		else if (jsonObj.type == 422) {
+			var html = '<tr><td></td><td>'+jsonObj.nickname+' left the channel</td></tr>';
+		}
+		else if (jsonObj.type == 412) {
+			var html = '<tr><td></td><td>'+jsonObj.nickname+' changed nickname to '+jsonObj.target_nick+'</td></tr>';
+		}
+	
+		$(html).prependTo('#messagelist');
+		$('#latestmessagets').val(jsonObj.timestamp);
+		lpStart();
+	};
+	 
+	var lpStart = function() {
+		var timestamp = $('#latestmessagets').val();
+		$.post('{% url /index.php/channel/ajax %}', {timestamp: timestamp}, lpOnComplete, 'json');
+	};
+	 
+	var lpReady = function() {
+		setTimeout("lpStart()", 1000);
+	}
+	
+	$(window).load(lpReady);
+</script>
+{% endblock %}
+
 {% block content %}
 
 <div id="left">
