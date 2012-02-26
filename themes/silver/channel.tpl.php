2de88f1a5b31550a2f18182ef92cd70aef8e10a0
Jack Harley <jackpharley@gmail.com>
2012-02-26 11:49:40 +0000

    Bye bye AJAX

--- themes/silver/channel.tpl.php
+++ themes/silver/channel.tpl.php
@@ -4,40 +4,6 @@
 
 {% block jsTwo %}
 <script type="text/javascript">
-	var lpOnComplete = function(response) {
-		var jsonObj = response;
-		if (jsonObj.type == 4412) {
-			if (jsonObj.nickname != " ") {
-				var html = '<tr><td><b><a href="{% url /index.php/stats/user %}/'+jsonObj.nickname+'">&lt;'+jsonObj.nickname+'&gt;</a></b></td><td>'+jsonObj.message+'</td></tr>';
-			}
-			else {
-				var html = '<tr><td></td><td>'+jsonObj.message+'</td></tr>';
-			}
-		}
-		else if (jsonObj.type == 421) {
-			var html = '<tr><td></td><td>'+jsonObj.nickname+' joined the channel</td></tr>';
-		}
-		else if (jsonObj.type == 422) {
-			var html = '<tr><td></td><td>'+jsonObj.nickname+' left the channel</td></tr>';
-		}
-		else if (jsonObj.type == 412) {
-			var html = '<tr><td></td><td>'+jsonObj.nickname+' changed nickname to '+jsonObj.target_nick+'</td></tr>';
-		}
-	
-		$(html).prependTo('#messagelist');
-		$('#latestmessagets').val(jsonObj.timestamp);
-		lpStart();
-	};
-	 
-	var lpStart = function() {
-		var timestamp = $('#latestmessagets').val();
-		$.post('{% url /index.php/channel/ajax %}', {timestamp: timestamp}, lpOnComplete, 'json');
-	};
-	 
-	var lpReady = function() {
-		setTimeout("lpStart()", 1000);
-	}
-	
 	var processOlderMessages = function(response) {
 		var data = response;
 		
@@ -70,8 +36,6 @@
 		var timestamp = $('#oldestmessagets').val();
 		$.post('{% url /index.php/channel/ajaxolder %}', {timestamp: timestamp}, processOlderMessages, 'json');
 	}
-	
-	$(window).load(lpReady);
 	
 	$(window).scroll(
 		function() {
