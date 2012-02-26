60fc348e8549c0187569d2ccd56bc2ce35fa798c
AwesomezGuy <jackpharley@gmail.com>
2012-02-26 18:15:32 +0000

    Removed the Hackulous theme, because maintaining it is a bitch

--- themes/hackulous/chat.tpl.php
+++
@@ -1,8 +0,0 @@
-{% extends base %}
-
-{% block pageTitle %}Webchat{% endblock %}
-
-{% block content %}
-<h1>Webchat</h1>
-<iframe width="100%" height="90%" src="http://widget.mibbit.com/?settings=5f7ba721eda38af8b5cafb392ec80c84&server={{address}}&channel={{channel|hashtohtml}}"></iframe>
-{% endblock %}
--- themes/hackulous/logs/messages.tpl.php
+++
@@ -1,3 +0,0 @@
-{% for message in messages %}
-	{{message.time|timetodate:"d M Y H:i:s"}} &lt;{{message.nickname}}&gt; {{message.message}}<br />
-{% endfor %}
